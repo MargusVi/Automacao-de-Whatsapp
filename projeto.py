6e924cffc1da3a25a44543bda38d1beabd3e58b0
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-12-06 15:06:54 -0300

    Abrindo o Whatsapp com o Chrome através do código

--- projeto.py
+++ projeto.py
@@ -1,4 +1,15 @@
 # Automação de encaminhamento de mensagens no Whatsapp
 # Usando a funcionalidade nativa do Whatsapp de encaminhar mensagens
 # Encaminhar de 5 em 5 mensagens
-# Selenium
+
+# Selenium -> permite fazer a automação
+# Pyperclip -> permite dar ctrl c / ctrl v em mensagens
+# Webdriver-manager -> conversa com o selenium
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+nav = webdriver.Chrome(service=service)
+nav.get("https://web.whatsapp.com")
