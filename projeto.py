05e76cdd987fdc256409c0a2ba4468408766b3d7
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-12-06 15:30:10 -0300

    Problema do Chrome fechando sozinho após abrir resolvido

--- projeto.py
+++ projeto.py
@@ -10,6 +10,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
 service = Service(ChromeDriverManager().install())
-nav = webdriver.Chrome(service=service)
+nav = webdriver.Chrome(options=options, service=service)
 nav.get("https://web.whatsapp.com")
